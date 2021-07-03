f2b0daf74bd3d9da057a092a31362cf6e8103579
iantatiana <TYan@croc.ru>
2021-07-03 20:05:38 +1000

    first commit

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        gameObject.transform.Rotate(new Vector3(45f, 45f, 45f) * Time.deltaTime);
+    }
+}
